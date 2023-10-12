a4ae6d4a4202843c2f81cffef0b697561ca2ff17
18chanp1 <89321535+18chanp1@users.noreply.github.com>
2023-10-12 01:02:53 -0700

    Implemented google sign in

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("com.android.application")
 }
 
+
+
 android {
     namespace = "com.beaker.recipeRoulette"
     compileSdk = 33
@@ -33,10 +35,17 @@ android {
 
 dependencies {
 
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.gms:play-services-base:18.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //OAuth
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+
+
 }
--- android/app/src/main/java/com/beaker/recipeRoulette/MainActivity.java
+++ android/app/src/main/java/com/beaker/recipeRoulette/MainActivity.java
@@ -1,14 +1,132 @@
 package com.beaker.recipeRoulette;
 
+
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.IntentSenderRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.app.Activity;
+
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.BeginSignInResult;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+
 
 public class MainActivity extends AppCompatActivity {
+    //Elements
+    private SignInButton signInButton;
+
+    // Handle Google Sign In
+    private SignInClient oneTapClient;
+    private BeginSignInRequest signInRequest;
+    private BeginSignInRequest signUpRequest;
+    private static final String TAG = "MainActivity";
+    //boolean showOneTapUI = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "creating");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signInButton = findViewById(R.id.sign_in_button);
+        setupSignIn();
+
+
+
+
     }
+
+    private void setupSignIn()
+    {
+        oneTapClient = Identity.getSignInClient(this);
+        signInRequest = BeginSignInRequest.builder()
+                .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
+                        .setSupported(false)
+                        .build())
+                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        // Your server's client IDs, not your Android client ID.
+                        .setServerClientId(getString(R.string.default_web_client_id))
+                        // Only show accounts previously used to sign in.
+                        .setFilterByAuthorizedAccounts(true)
+                        .build())
+                // Automatically sign in when exactly one credential is retrieved.
+                .setAutoSelectEnabled(true)
+                .build();
+        signUpRequest = BeginSignInRequest.builder()
+                .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
+                        .setSupported(false)
+                        .build())
+                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        // Your server's client IDs, not your Android client ID.
+                        .setServerClientId(getString(R.string.default_web_client_id))
+                        // Only show accounts previously used to sign in.
+                        .setFilterByAuthorizedAccounts(false)
+                        .build())
+                // Automatically sign in when exactly one credential is retrieved.
+                .setAutoSelectEnabled(true)
+                .build();
+
+
+        ActivityResultLauncher<IntentSenderRequest> activityResultLauncher =
+                registerForActivityResult(new ActivityResultContracts.StartIntentSenderForResult(), result -> {
+                    if(result.getResultCode() == Activity.RESULT_OK)
+                    {
+                        try {
+                            SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(result.getData());
+                            String idToken = credential.getGoogleIdToken();
+                            // String username = credential.getId();
+                            String password = credential.getPassword();
+                            if (idToken !=  null) {
+                                // Got an ID token from Google. Use it to authenticate
+                                // with your backend.
+                                Log.d(TAG, "Got ID token.");
+                            } else if (password != null) {
+                                // Got a saved username and password. Use them to authenticate
+                                // with your backend.
+                                Log.d(TAG, "Got password.");
+                            }
+                        } catch (ApiException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+
+        signInButton.setOnClickListener(view -> oneTapClient.beginSignIn(signInRequest)
+                .addOnSuccessListener(MainActivity.this, result -> {
+                    IntentSenderRequest intentSenderRequest =
+                            new IntentSenderRequest.Builder(result.getPendingIntent().getIntentSender()).build();
+                    activityResultLauncher.launch(intentSenderRequest);
+
+                })
+                .addOnFailureListener(MainActivity.this, e -> {
+                    // No saved credentials found. Launch the One Tap sign-up flow, or
+                    // do nothing and continue presenting the signed-out UI.
+                    Log.d(TAG, e.getLocalizedMessage());
+                    Log.d(TAG, "registering new user");
+
+                    oneTapClient.beginSignIn(signUpRequest);
+
+                    //TODO Handle case where user refuses to sign in.
+                }));
+
+
+    }
+
+
+
+
 }
