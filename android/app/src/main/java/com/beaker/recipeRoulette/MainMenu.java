6b0196870895c1b530d1e6fd5a0471aa16755c11
18chanp1 <89321535+18chanp1@users.noreply.github.com>
2023-10-12 13:46:11 -0700

    implemented a basic infinite scroll

--- android/app/src/main/java/com/beaker/recipeRoulette/MainMenu.java
+++ android/app/src/main/java/com/beaker/recipeRoulette/MainMenu.java
@@ -2,13 +2,26 @@ package com.beaker.recipeRoulette;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
+    private Button recipeReviewButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        recipeReviewButton = findViewById(R.id.menu5);
+
+        recipeReviewButton.setOnClickListener(view -> {
+            Intent mainMenuIntent = new Intent(MainMenu.this, RecipeFacebook.class);
+            startActivity(mainMenuIntent);
+        });
+
+
+
     }
 }
