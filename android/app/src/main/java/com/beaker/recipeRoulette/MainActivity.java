2c87f68a31e0e052a74edef045f2cb6c381704ac
18chanp1 <89321535+18chanp1@users.noreply.github.com>
2023-10-12 12:06:48 -0700

    auto sign in is now complete, token is saved accordingly

--- android/app/src/main/java/com/beaker/recipeRoulette/MainActivity.java
+++ android/app/src/main/java/com/beaker/recipeRoulette/MainActivity.java
@@ -6,6 +6,11 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.IntentSenderRequest;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.datastore.preferences.core.MutablePreferences;
+import androidx.datastore.preferences.core.Preferences;
+import androidx.datastore.preferences.core.PreferencesKeys;
+import androidx.datastore.preferences.rxjava3.RxPreferenceDataStoreBuilder;
+import androidx.datastore.rxjava3.RxDataStore;
 
 
 import android.app.Activity;
@@ -24,6 +29,8 @@ import com.google.android.gms.common.api.CommonStatusCodes;
 
 import java.util.Calendar;
 
+import io.reactivex.rxjava3.core.Single;
+
 
 public class MainActivity extends AppCompatActivity {
     //Elements
@@ -36,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private Calendar timeUntilNextSignIn;
     private static final int SIGNIN_TIMEOUT = 10;
+    private RxDataStore<Preferences> dataStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +53,9 @@ public class MainActivity extends AppCompatActivity {
 
         signInButton = findViewById(R.id.sign_in_button);
         setupSignIn();
+
+        //setup login state
+        dataStore = new RxPreferenceDataStoreBuilder(MainActivity.this,  "settings").build();
 
 
 
@@ -99,6 +110,20 @@ public class MainActivity extends AppCompatActivity {
                                 // Got an ID token from Google. Use it to authenticate
                                 // with your backend.
                                 Log.d(TAG, "Got ID token.");
+
+
+                                //Writing token and credentials to settings file
+                                Single<Preferences> updateResult = dataStore.updateDataAsync(prefsIn ->
+                                {
+                                    MutablePreferences mutablePreferences = prefsIn.toMutablePreferences();
+
+                                    Preferences.Key<String> tokKey = PreferencesKeys.stringKey("TOKEN");
+
+                                    mutablePreferences.set(tokKey, idToken);
+                                    return Single.just(mutablePreferences);
+                                });
+
+
                             } else if (password != null) {
                                 // Got a saved username and password. Use them to authenticate
                                 // with your backend.
@@ -117,7 +142,9 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
-        signInButton.setOnClickListener(view -> oneTapClient.beginSignIn(signInRequest)
+
+        //sign in automatically, otherwise, request register
+        oneTapClient.beginSignIn(signInRequest)
                 .addOnSuccessListener(MainActivity.this, result -> {
                     if(!timeUntilNextSignIn.after(Calendar.getInstance())) {
                         IntentSenderRequest intentSenderRequest =
@@ -144,7 +171,28 @@ public class MainActivity extends AppCompatActivity {
                         {
                             IntentSenderRequest intentSenderRequest = new IntentSenderRequest.Builder(result.getPendingIntent().getIntentSender()).build();
                             activityResultLauncher.launch(intentSenderRequest);
+                        }).addOnFailureListener(MainActivity.this, er ->
+                        {
+                            er.getCause();
+                            Toast toast = Toast.makeText(MainActivity.this,"You must be logged into a google account on android first", Toast.LENGTH_LONG);
+                            toast.show();
                         });
+                    }
+                    else {
+                        Log.d(TAG, "not showing one tap UI");
+                        String timeDiff = String.valueOf((timeUntilNextSignIn.getTimeInMillis() - Calendar.getInstance().getTimeInMillis()) / (1000 * 60));
+                        Toast toast = Toast.makeText(MainActivity.this,"You declined to sign in. You have to wait " + timeDiff + " minutes", Toast.LENGTH_LONG);
+                        toast.show();
+                    }});
+
+
+
+        signInButton.setOnClickListener(view -> oneTapClient.beginSignIn(signUpRequest)
+                .addOnSuccessListener(MainActivity.this, result -> {
+                    if(!timeUntilNextSignIn.after(Calendar.getInstance())) {
+                        IntentSenderRequest intentSenderRequest =
+                                new IntentSenderRequest.Builder(result.getPendingIntent().getIntentSender()).build();
+                        activityResultLauncher.launch(intentSenderRequest);
                     }
                     else {
                         Log.d(TAG, "not showing one tap UI");
@@ -153,11 +201,18 @@ public class MainActivity extends AppCompatActivity {
                         toast.show();
                     }
 
-
+                })
+                .addOnFailureListener(MainActivity.this, er -> {
+                    // No saved credentials found. Launch the One Tap sign-up flow, or
+                    // do nothing and continue presenting the signed-out UI.
+                    er.printStackTrace();
+                    Toast toast = Toast.makeText(MainActivity.this,"You must be logged into a google account on android first", Toast.LENGTH_LONG);
+                    toast.show();
                 }));
 
 
     }
+
 
 
 
