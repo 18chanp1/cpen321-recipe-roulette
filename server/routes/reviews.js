1c044ee56f6f63a3811b326cbd29d0f684789bb2
18chanp1 <89321535+18chanp1@users.noreply.github.com>
2023-10-12 15:28:22 -0700

    added interesting comments

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -12,6 +12,15 @@ router.get('/', function(req, res, next) {
       title: "How to cook human meat",
       stringDate: "2018-10-16",
       image: "https://ece.ubc.ca/files/2017/03/2016a-13-e1580928549507.jpeg"
+    },
+
+    {
+      id: "2",
+      rating: "5",
+      author: "Julia Rubin",
+      title: "How to spitroast human meat",
+      stringDate: "2018-10-16",
+      image: "https://ece.ubc.ca/files/2017/03/2016a-13-e1580928549507.jpeg"
     }
   ]
   res.send(reviews);
