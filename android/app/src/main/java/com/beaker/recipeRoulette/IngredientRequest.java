ec5819e88305588cc374fd687919dfa006f4a2c1
18chanp1 <89321535+18chanp1@users.noreply.github.com>
2023-10-20 15:45:04 -0700

    IngredientRequest type is done

--- android/app/src/main/java/com/beaker/recipeRoulette/IngredientRequest.java
+++ android/app/src/main/java/com/beaker/recipeRoulette/IngredientRequest.java
@@ -1,4 +1,122 @@
 package com.beaker.recipeRoulette;
 
-public class IngredientRequest {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+public class IngredientRequest implements Parcelable {
+    String reqID;
+    String reqDate;
+    String expiryDate;
+    String ingredientID;
+    String ingredientName;
+    String requestor;
+    String requestorID;
+
+    public IngredientRequest(String reqID, String reqDate, String expiryDate, String ingredientID, String ingredientName, String requestor, String requestorID) {
+        this.reqID = reqID;
+        this.reqDate = reqDate;
+        this.expiryDate = expiryDate;
+        this.ingredientID = ingredientID;
+        this.ingredientName = ingredientName;
+        this.requestor = requestor;
+        this.requestorID = requestorID;
+    }
+
+    protected IngredientRequest(Parcel in) {
+        reqID = in.readString();
+        reqDate = in.readString();
+        expiryDate = in.readString();
+        ingredientID = in.readString();
+        ingredientName = in.readString();
+        requestor = in.readString();
+        requestorID = in.readString();
+    }
+
+    public static final Creator<IngredientRequest> CREATOR = new Creator<IngredientRequest>() {
+        @Override
+        public IngredientRequest createFromParcel(Parcel in) {
+            return new IngredientRequest(in);
+        }
+
+        @Override
+        public IngredientRequest[] newArray(int size) {
+            return new IngredientRequest[size];
+        }
+    };
+
+    public String getReqID() {
+        return reqID;
+    }
+
+    public void setReqID(String reqID) {
+        this.reqID = reqID;
+    }
+
+    public String getReqDate() {
+        return reqDate;
+    }
+
+    public void setReqDate(String reqDate) {
+        this.reqDate = reqDate;
+    }
+
+    public String getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(String expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public String getIngredientID() {
+        return ingredientID;
+    }
+
+    public void setIngredientID(String ingredientID) {
+        this.ingredientID = ingredientID;
+    }
+
+    public String getIngredientName() {
+        return ingredientName;
+    }
+
+    public void setIngredientName(String ingredientName) {
+        this.ingredientName = ingredientName;
+    }
+
+    public String getRequestor() {
+        return requestor;
+    }
+
+    public void setRequestor(String requestor) {
+        this.requestor = requestor;
+    }
+
+    public String getRequestorID() {
+        return requestorID;
+    }
+
+    public void setRequestorID(String requestorID) {
+        this.requestorID = requestorID;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel parcel, int i) {
+
+        parcel.writeString(reqID);
+        parcel.writeString(reqDate);
+        parcel.writeString(expiryDate);
+        parcel.writeString(ingredientID);
+        parcel.writeString(ingredientName);
+        parcel.writeString(requestor);
+        parcel.writeString(requestorID);
+    }
+
 }
