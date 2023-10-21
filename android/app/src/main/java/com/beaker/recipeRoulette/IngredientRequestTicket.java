47ef2767c641f03cba047eb76d6f4b89f0955a02
18chanp1 <89321535+18chanp1@users.noreply.github.com>
2023-10-20 18:38:09 -0700

    the request ingredient function sends out appropriate posts

---
+++ android/app/src/main/java/com/beaker/recipeRoulette/IngredientRequestTicket.java
@@ -0,0 +1,11 @@
+package com.beaker.recipeRoulette;
+
+public class IngredientRequestTicket {
+    String tok;
+    String ingredient;
+
+    public IngredientRequestTicket(String tok, String ingredient) {
+        this.tok = tok;
+        this.ingredient = ingredient;
+    }
+}
--- android/app/src/main/java/com/beaker/recipeRoulette/IngredientRequestView.java
+++ android/app/src/main/java/com/beaker/recipeRoulette/IngredientRequestView.java
@@ -2,12 +2,19 @@ package com.beaker.recipeRoulette;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 
@@ -17,21 +24,33 @@ import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class IngredientRequestView extends AppCompatActivity {
+    private Button requestIngredientButton;
+    private Button viewRequestButton;
+    private EditText ingredientRequestText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ingredient_req_view);
 
+        //setup buttons
+        requestIngredientButton = findViewById(R.id.req_ingred_but);
+        viewRequestButton = findViewById(R.id.view_req_but);
+        ingredientRequestText = findViewById(R.id.ingredient_entry);
+
+        //get tokens
         SharedPreferences sharedPref =
                 this.getSharedPreferences(getString(R.string.shared_pref_filename), Context.MODE_PRIVATE);
         String tok = sharedPref.getString("TOKEN", "NOTOKEN");
 
+        //get requests from server
         Request req = new Request.Builder()
                 .url("https://cpen321-reciperoulette.westus.cloudapp.azure.com/ingredientrequests")
                 .addHeader("userToken", tok)
@@ -74,6 +93,66 @@ public class IngredientRequestView extends AppCompatActivity {
 
 
                 }
+            }
+        });
+
+        //setup the listener to submit requests
+
+        requestIngredientButton.setOnClickListener(view -> {
+            submitRequestHandler();
+        });
+
+        ingredientRequestText.setOnKeyListener((view, i, keyEvent) -> {
+            if(keyEvent.getAction() == KeyEvent.ACTION_DOWN && i == KeyEvent.KEYCODE_ENTER){
+                submitRequestHandler();
+            }
+            return true;
+        });
+
+    }
+
+    private void submitRequestHandler()
+    {
+        SharedPreferences sharedPref =
+                this.getSharedPreferences(getString(R.string.shared_pref_filename), Context.MODE_PRIVATE);
+        String tok = sharedPref.getString("TOKEN", "NOTOKEN");
+
+        OkHttpClient client = new OkHttpClient();
+
+        String foodReq = String.valueOf(ingredientRequestText.getText());
+        Gson gson = new Gson();
+        String json = gson.toJson(new IngredientRequestTicket(tok, foodReq));
+
+
+        MediaType JSON = MediaType.get("application/json; charset=utf-8");
+        RequestBody body = RequestBody.create(json, JSON);
+
+        Request postNewReq = new Request.Builder()
+                .url("https://cpen321-reciperoulette.westus.cloudapp.azure.com/ingredientrequests/new")
+                .addHeader("userToken", tok)
+                .addHeader("requestItem", foodReq)
+                .post(body)
+                .build();
+
+        client.newCall(postNewReq).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+                runOnUiThread(() -> {
+                    Toast toast = Toast.makeText(IngredientRequestView.this, "Request failed", Toast.LENGTH_LONG);
+                    toast.show();
+                });
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                runOnUiThread(() -> {
+                    if(response.isSuccessful()) {
+                        Log.d("test", "call ok");
+                        Toast toast = Toast.makeText(IngredientRequestView.this, "Request made", Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                });
             }
         });
     }
