7bf31214bab2da640318ad18f3cec649ec5d450b
18chanp1 <89321535+18chanp1@users.noreply.github.com>
2023-10-23 12:50:31 -0700

    move handlers outside

--- server/wss/wss.js
+++ server/wss/wss.js
@@ -7,16 +7,18 @@ function setupWSS(httpsServer)
     wss.on("connection", (ws) =>
     {
         console.log("connection");
-        ws.on("message", (message) =>
-        {
-            console.log("message: " + message);
-            ws.send("echo: " + message);
-        })
+        
+    })
 
-        ws.on("close", () =>
-        {
-            console.log("closed");
-        })
+    ws.on("message", (message) =>
+    {
+        console.log("message: " + message);
+        ws.send("echo: " + message);
+    })
+
+    ws.on("close", () =>
+    {
+        console.log("closed");
     })
 
     return wss;
