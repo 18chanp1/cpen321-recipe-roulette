cb1ab199ad18a2216e59cbb826f25e6349cd0731
JoshuaWu7 <joshua.wu2009@gmail.com>
2023-10-26 15:33:23 -0700

    add mongoDB connection code

--- server/app.js
+++ server/app.js
@@ -33,6 +33,15 @@ httpsServer.listen(8443, () =>
 	}
 )
 
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/test", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("We're connected to MongoDB!");
+});
+
 //setup websocket server
 let wss = require("./wss/wss")(httpsServer); 
 
