0f8c13df681802ed31a3e957ebfa862c9758f5b7
18chanp1 <89321535+18chanp1@users.noreply.github.com>
2023-11-01 20:46:12 -0700

    some morons spell "flavor" with a "u"

--- android/app/src/main/java/com/beaker/recipeRoulette/FlavorProfile.java
+++ android/app/src/main/java/com/beaker/recipeRoulette/FlavorProfile.java
@@ -44,7 +44,7 @@ public class FlavorProfile extends AppCompatActivity {
 
         //get requests from server
         Request req = new Request.Builder()
-                .url("https://cpen321-reciperoulette.westus.cloudapp.azure.com/flavorprofile")
+                .url("https://cpen321-reciperoulette.westus.cloudapp.azure.com/flavourprofile")
                 .addHeader("email", email)
                 .addHeader("userToken", tok)
                 .build();
