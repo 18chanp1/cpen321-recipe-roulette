887182cc35ad4a51f58fdbff4a963a646aee830e
18chanp1 <89321535+18chanp1@users.noreply.github.com>
2023-11-11 18:08:33 -0800

    added tests that review in detail

--- android/app/src/androidTest/java/com/beaker/reciperoulette/ReviewsTest.java
+++ android/app/src/androidTest/java/com/beaker/reciperoulette/ReviewsTest.java
@@ -2,10 +2,16 @@ package com.beaker.reciperoulette;
 
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.action.ViewActions.swipeDown;
+import static androidx.test.espresso.action.ViewActions.swipeUp;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
@@ -13,10 +19,12 @@ import static org.junit.Assert.fail;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
 
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
@@ -37,7 +45,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-import static com.beaker.reciperoulette.RecyclerViewActions.*;
+import static com.beaker.reciperoulette.RecyclerViewActions.scrollToPosition;
 
 
 @LargeTest
@@ -53,14 +61,10 @@ public class ReviewsTest {
     @Test
     public void testReviewsEntry() {
         setup();
-
         RecyclerViewMatcher rvm = new RecyclerViewMatcher(R.id.recyclerView);
-
-
 
         //Get context
         Context c = InstrumentationRegistry.getInstrumentation().getTargetContext();
-
 
         //get token
         SharedPreferences sharedPref =
@@ -76,31 +80,21 @@ public class ReviewsTest {
                 .build();
 
         OkHttpClient client = new OkHttpClient();
-
         try (Response response = client.newCall(req).execute())
         {
             if(response.isSuccessful())
             {
                 String res = response.body().string();
-
-                Log.d("RecipeFacebook", res);
-
                 Review[] userArray = new Gson().fromJson(res, Review[].class);
-
-                List<Review> reviews = new ArrayList<Review>();
-
 
                 for(int i = 0; i < userArray.length; i++)
                 {
-                    Matcher<View> currentMatch = new RecyclerViewMatcher(R.id.recyclerView).atPosition(i);
-
                     onView(withId(R.id.recyclerView))
                             .perform(scrollToPosition(i));
-
+                    Matcher<View> currentMatch = new RecyclerViewMatcher(R.id.recyclerView).atPosition(i);
                     onView(currentMatch)
                             .check(matches(hasDescendant(withText(userArray[i].title))));
                 }
-
             }
             else
             {
@@ -109,10 +103,108 @@ public class ReviewsTest {
 
 
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            fail("Failed to get list");
         }
 
     }
+
+    @Test
+    public void testReviewsDetailed() {
+        setup();
+        RecyclerViewMatcher rvm = new RecyclerViewMatcher(R.id.recyclerView);
+
+        //Get context
+        Context c = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        //get token
+        SharedPreferences sharedPref =
+                c.getSharedPreferences(c.getString(R.string.shared_pref_filename), Context.MODE_PRIVATE);
+        String tok = sharedPref.getString("TOKEN", "NOTOKEN");
+        String email = sharedPref.getString("EMAIL", "NOEMAIL");
+
+        // fetch the list of items
+        Request req = new Request.Builder()
+                .url("https://cpen321-reciperoulette.westus.cloudapp.azure.com/reviews")
+                .addHeader("email", email)
+                .addHeader("userToken", tok)
+                .build();
+
+        OkHttpClient client = new OkHttpClient();
+        try (Response response = client.newCall(req).execute()) {
+            if (response.isSuccessful()) {
+                String res = response.body().string();
+                Review[] userArray = new Gson().fromJson(res, Review[].class);
+
+                for (int i = 0; i < userArray.length; i++) {
+                    onView(withId(R.id.recyclerView))
+                            .perform(scrollToPosition(i));
+                    Matcher<View> currentMatch = new RecyclerViewMatcher(R.id.recyclerView).atPosition(i);
+                    //click the read more button;
+                    onView(allOf(withParent(currentMatch), withId(R.id.readmore_but)))
+                            .perform(click());
+
+                    //assert that the title is the same
+                    onView(withId(R.id.detailed_title))
+                            .check(matches(isDisplayed()))
+                            .check(matches(withText(userArray[i].title)));
+
+                    //assert that contact is the same
+                    onView(withId(R.id.detailed_author))
+                            .check(matches(isDisplayed()))
+                            .check(matches(withText(userArray[i].author)));
+
+                    //assert that rating is the same
+                    onView(withId(R.id.detailed_rating))
+                            .check(matches(isDisplayed()))
+                            .check(matches(withText("Rating: " + userArray[i].rating)));
+
+                    //assert that the review is the same
+                    String review = Html.fromHtml(userArray[i].review, Html.FROM_HTML_MODE_COMPACT).toString();
+                    onView(withId(R.id.detailed_text))
+                            .check(matches(isDisplayed()))
+                            .check(matches(withText(review)));
+
+                    //assert that like buttons work
+                    onView(withId(R.id.like_button))
+                            .perform(scrollTo())
+                            .check(matches(isDisplayed()))
+                            .check(matches(withText(R.string.like)));
+
+                    //test the like button
+                    onView(withId(R.id.like_button))
+                            .perform(click());
+
+                    //go back to refresh item
+                    pressBack();
+                    pressBack();
+                    onView(withText("Recipe Reviews")).perform(click());
+                    onView(withId(R.id.recyclerView))
+                            .perform(scrollToPosition(i));
+                    currentMatch = new RecyclerViewMatcher(R.id.recyclerView).atPosition(i);
+                    onView(allOf(withParent(currentMatch), withId(R.id.readmore_but)))
+                            .perform(click());
+
+                    //check that like count has increased
+                    String newRating = String.valueOf(Integer.valueOf(userArray[i].rating + 1)) ;
+                    onView(withId(R.id.detailed_rating))
+                            .check(matches(isDisplayed()))
+                            .check(matches(withText("Rating: " + newRating)));
+
+                    pressBack();
+
+
+                }
+            } else {
+                fail("Entry does not match");
+            }
+
+
+        } catch (IOException e) {
+            fail("Failed to get list");
+        }
+    }
+
+
 
     private static void setup()
     {
