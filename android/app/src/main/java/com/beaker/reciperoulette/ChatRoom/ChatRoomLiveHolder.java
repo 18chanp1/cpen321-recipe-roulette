035f90be6ca30b5eb99fa8905d5dbce46e52d445
18chanp1 <89321535+18chanp1@users.noreply.github.com>
2023-11-26 11:34:39 -0800

    ChatRoomLiveHolder cleaned up

--- android/app/src/main/java/com/beaker/reciperoulette/ChatRoom/ChatRoomLiveHolder.java
+++ android/app/src/main/java/com/beaker/reciperoulette/ChatRoom/ChatRoomLiveHolder.java
@@ -1,7 +1,6 @@
 package com.beaker.reciperoulette.ChatRoom;
 
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,7 +13,6 @@ public class ChatRoomLiveHolder extends RecyclerView.ViewHolder {
     ImageView imageView;
     TextView nameView;
     TextView detailedView;
-    Button Details;
 
     public ChatRoomLiveHolder(@NonNull View itemView) {
         super(itemView);
