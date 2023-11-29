b0ae5e5e541853baacb5f120200ae96bd1f3ea14
Theo1611 <nguyenteo2u@gmail.com>
2023-11-28 22:28:49 -0800

    changed the script

--- server/server_check.sh
+++ server/server_check.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-a=$(ps -a | grep "npm start")
-echo ${a}
 while true
 do
 	while [ "$(ps -a | grep "node")" != "" ]
@@ -8,6 +6,7 @@ do
 		echo "$(ps -a | grep "node")"
 		sleep 5
 	done
+	date >> public/crashes.txt
 	screen -dm bash -c "npm start"
 	sleep 5
 done
